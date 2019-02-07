66b972f8f8d157a4701061a63b926951d1559d30
JC <joaocarlosc.m@hotmail.com>
2019-02-07 17:23:11 -0200

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,6 +5,8 @@
  */
 package boardgame;
 
+import chess.pieces.Rook;
+
 /**
  *
  * @author W7u
@@ -43,4 +45,10 @@ public class Board {
     public Piece piece(Position position){
         return pieces[position.getRow()][position.getColumn()];
     }
+    
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+
 }
---
+++ src/chess/pieces/King.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+/**
+ *
+ * @author W7u
+ */
+public class King extends ChessPiece {
+    
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+    
+    public String toString(){
+        return "K";
+    }
+}
